772af1450fbe8e81772fffb6e341b9da254c1297
vivekrawat21 <vivekrwt2111@gmail.com>
2024-01-31 22:28:17 +0530

    done with custom react

--- projectone/src/App.jsx
+++ projectone/src/App.jsx
@@ -1,9 +1,25 @@
 
 function App() {
+  const username = "vivek rawat";
  return(
   <>
   <div>
-    <h1>hello world</h1></div>
+    <h1>hello world{username /*We can only use evaluated expression here because actually the variable injected after creating the whole tree in the main.jsx it passes last of the function createElement of react 
+    and it is like a object and we cannnot make a object a statement so we only have to pass the evaluated expression means which value has been previously evaluated and cannot be the if or else if statements 
+     */}</h1></div>
+
+     {/* summary of this video : first we have created an dom like structure using custom react 
+     so  after we make a modular and more reusable approach of that using loops which is modular approact
+     after we make   that custom reder method we created we pass the value with that specific approach and get the result
+     so as we know that app.jsx is a nethod can't we just pass the method directly into the react rendor method we can obviously do that
+     so after that we passes our custom element to the react.render function return but that do not works because the methods are not same as which are written in the behind the scenes in the react
+     for that we have created a custom element which can be passed
+      after that we created a react elelment by the help of React.createElement which takes several arguments and have their own key values which after passing are showned on the live
+      by this we evaluate the react.createElement is automatically injected by the babel transpiler and finally we can see it in the window localhost in that function we first pass the tagname then  pass the attribute and after passing the attribute we pass the children and after variables at the last as an object 
+      after that we see that how a varible is injected in the createReactElelment function it is injected after the children and  properties pases to the fuction and because it is an object we cannot pass some  unevaluated value llike if else statement we can only pass the pre evaluated expression 
+      we can directly call the jsx element and we can do directly manipulation without even calling the render but this thing sometimes tricky and have many things inside that deep so we do not prefer to do that 
+      All this thing is present in the react open source... repositry we can see that there */}
+     
   </>
  )
  
--- projectone/src/main.jsx
+++ projectone/src/main.jsx
@@ -2,8 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp() {
+//   return(
+//     <div>
+//       custom app
+//     </div>
+//   )
+// }
+  // <React.StrictMode>
+  //   {/* <MyApp /> */}
+  //   {/* we can also use MyApp()  to do this work */}
+  //   {/* <App /> */}
+   
+  // </React.StrictMode>,
+// const customElement = (
+//   <div>
+//   <a href="https://www.google.com">click to go to google</a>
+//   </div>
+// )
+
+// const reactElement = React.createElement(
+//   // 'tag' first parameter is tag
+//   'a',
+//   {
+//     href: 'https://www.google.com',
+//     target: '_blank',
+//   },
+//   'click me to visit google' //This is the function which runs behid the scene with customizations...
+// )
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   {/* <MyApp /> */}
+  //   {/* we can also use MyApp()  to do this work */}
+     <App /> 
+   
+  // </React.StrictMode>,
+  // customElement
+  //from above line we can say that the react strict mode prohibit the customEelement
+  //In react there is a createElement fucntion is present which changes the jsx code into a react dom js
+  // reactElement
 )
