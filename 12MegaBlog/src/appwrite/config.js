430e1ffabf0b9278f0ff7991393ffc1f688ffbd1
vivekra21 <vivekrwt2111@gmail.com>
2024-03-27 12:02:33 +0530

    Some components done

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -1,5 +1,5 @@
 import conf from "../conf/conf.js";
-import {Client , ID , Databases , Storage , Query} from 'appwrite';
+import {Client , ID , Databases , Storage , Query,Account} from 'appwrite';
 
 export class Service{
     client = new Client();
@@ -18,7 +18,7 @@ export class Service{
 
     }
 
-    async createPost({title ,slug, title, content,featuredImage ,status , userId}){
+    async createPost({slug, title, content,featuredImage ,status , userId}){
         try {
             return await this.databases.createDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,  {
                 title,
