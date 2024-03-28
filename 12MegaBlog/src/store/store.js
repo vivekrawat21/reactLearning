ed023433bf9422971c5804ba84e8ae161c7d30b4
vivekra21 <vivekrwt2111@gmail.com>
2024-03-29 00:02:11 +0530

    Add authReducer to store.js

--- 12MegaBlog/src/store/store.js
+++ 12MegaBlog/src/store/store.js
@@ -1,9 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
+import authReducer from '../store/authSlice';
 
 const store = configureStore({
-    reducer:{
-
-    }
+    reducer: authReducer
     
 });
 export default store;
